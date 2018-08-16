cc82177cc911b15b3e1d41b3637f847e75c49360
Judy Saunders <segwyne@gmail.com>
2018-08-16 00:09:30 -0400

    Worked on slideshows some more

--- showcase.php
+++ showcase.php
@@ -3,60 +3,89 @@
 <h1>Showcase</h1>
 <p>Here are some gratuitous photos of fall in my town.</p>
 <!-- First slideshow -->
-<div class="slideshow">
+<div class="slideshow portrait">
     <div class="my-slides">
-        <div class="numbertext">1/5</div>
+        <div class="numbertext">1 / 5</div>
         <img src="slideshow/red-tree-1.jpg" alt="" />
         <div class="caption">Red tree 1</div>
     </div>
     <div class="my-slides">
-        <div class="numbertext">2/5</div>
+        <div class="numbertext">2 / 5</div>
         <img src="slideshow/red-tree-2.jpg" alt="" />
         <div class="caption">Red tree 2</div>
     </div>
     <div class="my-slides">
-        <div class="numbertext">3/5</div>
+        <div class="numbertext">3 / 5</div>
         <img src="slideshow/red-tree-3.jpg" alt="" />
         <div class="caption">Red tree 3</div>
     </div>
     <div class="my-slides">
-        <div class="numbertext">4/5</div>
+        <div class="numbertext">4 / 5</div>
         <img src="slideshow/red-tree-4.jpg" alt="" />
         <div class="caption">Red tree 4</div>
     </div>
     <div class="my-slides">
-        <div class="numbertext">5/5</div>
+        <div class="numbertext">5 / 5</div>
         <img src="slideshow/red-tree-5.jpg" alt="" />
         <div class="caption">Red tree 5</div>
     </div>
-    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+    <a class="prev" onclick="plusDivs(-1, 0)">&#10094;</a>
+    <a class="next" onclick="plusDivs(1, 0)">&#10095;</a>
+  
+</div>
+
+<!-- Second slideshow -->
+<div class="slideshow landscape">
+    <div class="my-slides-1">
+        <div class="numbertext">1 / 5</div>
+        <img src="slideshow/apples-3.jpg" alt="" />
+        <div class="caption">Apple orchard</div>
+    </div>
+    <div class="my-slides-1">
+        <div class="numbertext">2 / 5</div>
+        <img src="slideshow/fall-bog-3.jpg" alt="" />
+        <div class="caption">Bog</div>
+    </div>
+    <div class="my-slides-1">
+        <div class="numbertext">3 / 5</div>
+        <img src="slideshow/fall-intersection-2.jpg" alt="" />
+        <div class="caption">Highway intersection</div>
+    </div>
+    <div class="my-slides-1">
+        <div class="numbertext">4 / 5</div>
+        <img src="slideshow/fall-village-1.jpg" alt="" />
+        <div class="caption">Quaint village</div>
+    </div>
+    <div class="my-slides-1">
+        <div class="numbertext">5 / 5</div>
+        <img src="slideshow/fall-roadside-2.jpg" alt="" />
+        <div class="caption">Roadside</div>
+    </div>
+    <a class="prev" onclick="plusDivs(-1, 1)">&#10094;</a>
+    <a class="next" onclick="plusDivs(1, 1)">&#10095;</a>
   
 </div>
 
 <!-- Blatantly copy-and-pasted since I am nowhere near ready to understand JavaScript yet -->
 <script>
-var slideIndex = 1;
-showSlides(slideIndex);
+var slideIndex = [1,1];
+var slideId = ["my-slides", "my-slides-1"]
+showDivs(1, 0);
+showDivs(1, 1);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusDivs(n, no) {
+  showDivs(slideIndex[no] += n, no);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
+function showDivs(n, no) {
   var i;
-  var slides = document.getElementsByClassName("my-slides");    /* tweaked to match the name I used */
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  x[slideIndex[no]-1].style.display = "block";  
 }
 </script>
 
