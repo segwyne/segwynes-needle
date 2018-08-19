5bb2b24d06545e14826229d246e906534b7952f1
Judy Saunders <segwyne@gmail.com>
2018-08-18 23:50:09 -0400

    Silliness with rolling photos

---
+++ cssplay.php
@@ -0,0 +1,19 @@
+<?php include('header.php') ?>
+
+<div id="content" class="clearfix">
+
+<div>
+<h1>Just because I can</h1>
+</div>
+<div class="roll">
+<!-- base image -->
+<img src="profile.jpg" alt="" width="150"/>
+</div> <!-- roll -->
+<br /><br />
+<div class="box">
+<!-- base image -->
+<img src="profile.jpg" alt="" width="150"/>
+</div> <!-- box -->
+
+</div> <!-- #content -->
+<?php include('footer.php') ?>
